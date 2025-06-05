0c58dfb450546f04b997ad4e5ce57a796552e479
patapelada <77882237+patapelada@users.noreply.github.com>
2025-06-05 10:29:10 +0200

    feat: release workflow

--- src/quarto_lib/__init__.py
+++ src/quarto_lib/__init__.py
@@ -1,3 +1,5 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from quarto_lib.arena import Arena
 from quarto_lib.contracts.game_state import GameState
 from quarto_lib.contracts.informal_agent_interface import InformalAgentInterface
@@ -6,6 +8,12 @@ from quarto_lib.tournament_round import TournamentRound
 from quarto_lib.types.cell import Cell
 from quarto_lib.types.piece import Piece
 from quarto_lib.utils import check_win, common_characteristics, get_all_lines, piece_to_parts
+
+try:
+    __version__ = version("package-name")
+except PackageNotFoundError:
+    # package is not installed
+    pass
 
 __all__ = [
     "Arena",
